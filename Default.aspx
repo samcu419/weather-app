f4700b30cd2462a74be2000d0f8618f9fcf078cf
samcu419 <ndpack1@hotmail.com>
2015-10-01 13:39:37 -0400

    first commit

---
+++ Default.aspx
@@ -0,0 +1,140 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WeatherApplication2._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:WeatherAppDBASEConnectionString %>" SelectCommand="SELECT * FROM [Table]"></asp:SqlDataSource>
+            <hgroup class="title">
+                <h2>Enter a City and State:</h2>
+            </hgroup>
+            <p>
+                <asp:TextBox ID="CityTextBox" runat="server" ToolTip="Type a city"></asp:TextBox>
+                <asp:DropDownList ID="StateDropBox" runat="server">
+                    <asp:ListItem>AL</asp:ListItem>
+                    <asp:ListItem>AK</asp:ListItem>
+                    <asp:ListItem>AZ</asp:ListItem>
+                    <asp:ListItem>AR</asp:ListItem>
+                    <asp:ListItem>CA</asp:ListItem>
+                    <asp:ListItem>CO</asp:ListItem>
+                    <asp:ListItem>CT</asp:ListItem>
+                    <asp:ListItem>DE</asp:ListItem>
+                    <asp:ListItem>FL</asp:ListItem>
+                    <asp:ListItem>GA</asp:ListItem>
+                    <asp:ListItem>HI</asp:ListItem>
+                    <asp:ListItem>ID</asp:ListItem>
+                    <asp:ListItem>IL</asp:ListItem>
+                    <asp:ListItem>IN</asp:ListItem>
+                    <asp:ListItem>IA</asp:ListItem>
+                    <asp:ListItem>KS</asp:ListItem>
+                    <asp:ListItem>KY</asp:ListItem>
+                    <asp:ListItem>LA</asp:ListItem>
+                    <asp:ListItem>ME</asp:ListItem>
+                    <asp:ListItem>MD</asp:ListItem>
+                    <asp:ListItem>MA</asp:ListItem>
+                    <asp:ListItem>MI</asp:ListItem>
+                    <asp:ListItem>MN</asp:ListItem>
+                    <asp:ListItem>MS</asp:ListItem>
+                    <asp:ListItem>MO</asp:ListItem>
+                    <asp:ListItem>MT</asp:ListItem>
+                    <asp:ListItem>NE</asp:ListItem>
+                    <asp:ListItem>NV</asp:ListItem>
+                    <asp:ListItem>NH</asp:ListItem>
+                    <asp:ListItem>NJ</asp:ListItem>
+                    <asp:ListItem>NM</asp:ListItem>
+                    <asp:ListItem>NY</asp:ListItem>
+                    <asp:ListItem>NC</asp:ListItem>
+                    <asp:ListItem>ND</asp:ListItem>
+                    <asp:ListItem>OH</asp:ListItem>
+                    <asp:ListItem>OK</asp:ListItem>
+                    <asp:ListItem>OR</asp:ListItem>
+                    <asp:ListItem>PA</asp:ListItem>
+                    <asp:ListItem>RI</asp:ListItem>
+                    <asp:ListItem>SC</asp:ListItem>
+                    <asp:ListItem>SD</asp:ListItem>
+                    <asp:ListItem>TN</asp:ListItem>
+                    <asp:ListItem>TX</asp:ListItem>
+                    <asp:ListItem>UT</asp:ListItem>
+                    <asp:ListItem>VT</asp:ListItem>
+                    <asp:ListItem>VA</asp:ListItem>
+                    <asp:ListItem>WA</asp:ListItem>
+                    <asp:ListItem>WV</asp:ListItem>
+                    <asp:ListItem>WI</asp:ListItem>
+                    <asp:ListItem>WY</asp:ListItem>
+                </asp:DropDownList>
+            </p>
+            <p>
+                <asp:Button ID="GetWeatherBtn" runat="server" Text="Get Weather" Width="123px" OnClick="GetWeatherBtn_Click" />
+                <asp:Button ID="SaveLocationBtn" runat="server" Text="Save Location" Width="140px" OnClick="SaveLocationBtn_Click" />
+            </p>
+            <hgroup class="title">
+                <h2>Or Select One That You Have Saved:</h2>
+            </hgroup>
+            <p>
+                <asp:TextBox ID="SignInTextBox" runat="server"></asp:TextBox>
+                <asp:Button ID="SignInButton" runat="server" Text="Sign in" Height="32px" OnClick="SignInButton_Click" />
+            </p>
+            <p>
+                <asp:TextBox ID="CreateNewTextBox" runat="server"></asp:TextBox>
+                <asp:Button ID="CreateNewButton" runat="server" Text="Create New" Height="32px" OnClick="CreateNewButton_Click" />
+            </p>
+            <p>
+                <asp:DropDownList ID="SavedLocations" runat="server"></asp:DropDownList>
+                <asp:Button ID="GetWeatherBtn2" runat="server" Text="Get Weather" Width="123px" OnClick="GetWeatherBtn2_Click" Height="32px" />
+            </p>
+            <p>
+                <asp:Button ID="SignOutButton" runat="server" Text="Sign Out" Height="32px" OnClick="SignOutButton_Click" />
+            </p>
+            <p>
+                <asp:Label ID="ResultLabel" runat="server" Text="N/A"></asp:Label>
+            </p>
+            <p>
+                <asp:Label ID="SignedInLabel" runat="server" Text="" BorderColor="Black" BorderStyle="Groove"></asp:Label>
+            </p>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <asp:Label ID="Label1" runat="server" Text="Weather For"></asp:Label>
+    <ol class="round">
+        <li>
+            <h5>Place</h5>
+            <asp:Label ID="ResultsLbl1" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Observation Time</h5>
+            <asp:Label ID="ResultsLbl2" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Weather</h5>
+            <asp:Label ID="ResultsLbl3" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Temperature</h5>
+            <asp:Label ID="ResultsLbl4" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Relative Humidity</h5>
+            <asp:Label ID="ResultsLbl5" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Wind</h5>
+            <asp:Label ID="ResultsLbl6" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Pressure (mb)</h5>
+            <asp:Label ID="ResultsLbl7" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Dewpoint</h5>
+            <asp:Label ID="ResultsLbl8" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Visibility (km)</h5>
+            <asp:Label ID="ResultsLbl9" runat="server" Text="N/A"></asp:Label>
+        </li>
+        <li>
+            <h5>Location</h5>
+            <asp:Label ID="ResultsLbl10" runat="server" Text="N/A"></asp:Label>
+        </li>
+    </ol>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace WeatherApplication2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void SignOutButton_Click(object sender, EventArgs e)
+        {
+            SignInTextBox.Text = "";
+            CreateNewTextBox.Text = "";
+            SavedLocations.Items.Clear();
+            SignedInLabel.Text = "";
+        }
+
+        protected void SignInButton_Click(object sender, EventArgs e)
+        {
+            var list = new List<string>();
+            SqlConnection cn = new SqlConnection
+                    (ConfigurationManager.ConnectionStrings["WeatherAppDBASEConnectionString"]
+                    .ConnectionString);
+            cn.Open();
+            string checkQuery = "SELECT COUNT(*) FROM [Table] WHERE ([username] = @username)";
+            SqlCommand cmd = new SqlCommand(checkQuery, cn);
+            cmd.Parameters.AddWithValue("@username", SignInTextBox.Text);
+            int temp = (int)cmd.ExecuteScalar();
+
+            if (temp > 0)
+            {
+                //Name already exists
+                ResultLabel.Text = "THE NAME EXISTS";
+                SavedLocations.Items.Clear();
+
+                //loadsavedlocations//
+                string getLocationsQuery = "SELECT [location1],[location2],[location3] FROM [Table] WHERE ([username] = @username)";
+
+                using (var command = new SqlCommand(getLocationsQuery, cn))
+                {
+                    command.Parameters.AddWithValue("@username", SignInTextBox.Text.ToLower());
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(reader.GetString(0));
+                            list.Add(reader.GetString(1));
+                            list.Add(reader.GetString(2));
+                        }
+                    }
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    SavedLocations.Items.Add(list[i]);
+                }
+
+                SignedInLabel.Text = SignInTextBox.Text.ToLower();
+            }
+            else
+            {
+                //Name does not exist
+                ResultLabel.Text = "THE NAME DOES NOT EXIST";
+            }
+            
+            cn.Close();
+        }
+
+        protected void CreateNewButton_Click(object sender, EventArgs e)
+        {
+            var list = new List<string>();
+            SqlConnection cn = new SqlConnection
+                    (ConfigurationManager.ConnectionStrings["WeatherAppDBASEConnectionString"]
+                    .ConnectionString);
+            cn.Open();
+            string checkQuery = "SELECT COUNT(*) FROM [Table] WHERE ([username] = @username)";
+            SqlCommand cmd = new SqlCommand(checkQuery, cn);
+            cmd.Parameters.AddWithValue("@username", CreateNewTextBox.Text);
+            int temp = (int)cmd.ExecuteScalar();
+
+            if (temp > 0)
+            {
+                //Name already exists
+                ResultLabel.Text = "THE NAME EXISTS";
+            }
+            else
+            {
+                //Name does not exist
+                ResultLabel.Text = "THE NAME DOES NOT EXIST";
+                SavedLocations.Items.Clear();
+
+                //insert new name into database//
+                string insertQuery = "INSERT INTO [Table] ([username],[location1],[location2],[location3]) VALUES (@user, @val1, @val2, @val3)";
+                using (SqlCommand comm = new SqlCommand(insertQuery, cn))
+                {
+                    comm.Parameters.AddWithValue("@user", CreateNewTextBox.Text.ToLower());
+                    comm.Parameters.AddWithValue("@val1", " ");
+                    comm.Parameters.AddWithValue("@val2", " ");
+                    comm.Parameters.AddWithValue("@val3", " ");
+                    comm.ExecuteNonQuery();
+                }
+
+                //loadsavedlocations//
+                string getLocationsQuery = "SELECT [location1],[location2],[location3] FROM [Table] WHERE ([username] = @username)";
+
+                using (var command = new SqlCommand(getLocationsQuery, cn))
+                {
+                    command.Parameters.AddWithValue("@username", CreateNewTextBox.Text.ToLower());
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(reader.GetString(0));
+                            list.Add(reader.GetString(1));
+                            list.Add(reader.GetString(2));
+                        }
+                    }
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    SavedLocations.Items.Add(list[i]);
+                }
+
+                SignedInLabel.Text = CreateNewTextBox.Text.ToLower();
+            }
+
+            cn.Close();
+        }
+
+        protected void SaveLocationBtn_Click(object sender, EventArgs e)
+        {
+            if (SignedInLabel.Text != "")
+            {
+                string locationToSave = CityTextBox.Text.Replace(' ', '_') + " " + StateDropBox.SelectedItem.Value.ToString();
+                var list = new List<string>();
+
+                //open connection
+                SqlConnection cn = new SqlConnection
+                    (ConfigurationManager.ConnectionStrings["WeatherAppDBASEConnectionString"]
+                    .ConnectionString);
+                cn.Open();
+
+                //loadcurrentlocations//
+                string getCurrentQuery = "SELECT [location1],[location2],[location3] FROM [Table] WHERE ([username] = @username)";
+
+                using (var command = new SqlCommand(getCurrentQuery, cn))
+                {
+                    command.Parameters.AddWithValue("@username", SignedInLabel.Text);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(reader.GetString(0));
+                            list.Add(reader.GetString(1));
+                            list.Add(reader.GetString(2));
+                        }
+                    }
+                }
+
+                //save location to database
+                string updateQuery = "UPDATE [Table] SET [location1]=@val1, [location2]=@val2, [location3]=@val3 WHERE [username]=@username";
+                using (SqlCommand cmd = new SqlCommand(updateQuery, cn))
+                {
+                    cmd.Parameters.AddWithValue("@username", SignedInLabel.Text);
+                    cmd.Parameters.AddWithValue("@val3", list[1]);
+                    cmd.Parameters.AddWithValue("@val2", list[0]);
+                    cmd.Parameters.AddWithValue("@val1", locationToSave);
+                    cmd.ExecuteNonQuery();
+                }
+
+                //loadsavedlocations//
+                SavedLocations.Items.Clear();
+                list.Clear();
+                string getLocationsQuery = "SELECT [location1],[location2],[location3] FROM [Table] WHERE ([username] = @username)";
+
+                using (var command = new SqlCommand(getLocationsQuery, cn))
+                {
+                    command.Parameters.AddWithValue("@username", SignedInLabel.Text);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(reader.GetString(0));
+                            list.Add(reader.GetString(1));
+                            list.Add(reader.GetString(2));
+                        }
+                    }
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    SavedLocations.Items.Add(list[i]);
+                }
+
+
+                //refresh dropdown list//
+                SavedLocations.Items.Add(locationToSave);
+
+                cn.Close();
+            }
+        }
+
+        protected void GetWeatherBtn_Click(object sender, EventArgs e)
+        {
+            //Start Pulling Weather Code
+            string wundergroundAPI_key = "244dbe78d6953de3";
+            string state = StateDropBox.SelectedItem.Value.ToString();
+            string city = CityTextBox.Text.Replace(' ', '_');
+
+            //Calls the dissection method with the proper constructed request
+            parse("http://api.wunderground.com/api/" + wundergroundAPI_key + "/conditions/q/" + state + "/" + city + ".xml");
+        }
+
+        protected void GetWeatherBtn2_Click(object sender, EventArgs e)
+        {
+            //Start Pulling Weather Code
+            string wundergroundAPI_key = "244dbe78d6953de3";
+            string[] locationArray = SavedLocations.SelectedItem.Value.ToString().Split(' ');
+            string city = locationArray[0];
+            string state = locationArray[1];
+
+            //Calls the dissection method with the proper constructed request
+            parse("http://api.wunderground.com/api/" + wundergroundAPI_key + "/conditions/q/" + state + "/" + city + ".xml");
+        }
+
+        //Takes urlxml request, downloads it, parses it, and displays the parsed data
+        private void parse(string urlxml)
+        {
+            //Variables that will be parsed from the api
+            string place = "";
+            string observation_time = "";
+            string weather1 = "";
+            string temperature_string = "";
+            string relative_humidity = "";
+            string wind_string = "";
+            string pressure_mb = "";
+            string dewpoint_string = "";
+            string visibility_km = "";
+            string latitude = "";
+            string longitude = "";
+
+            //Declares client and downloads the urlxml for parsing
+            var client = new WebClient();
+            string urlRequest = client.DownloadString(urlxml);
+
+            //An xmlreader is assigned to the inputted urlxml
+            using (XmlReader reader = XmlReader.Create(new StringReader(urlRequest)))
+            {
+                //Read data from the reader
+                while (reader.Read())
+                {
+                    //For all the nodes in the reader, if the NodeType is Element; perform the following cases to parse the data
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (reader.Name.Equals("full"))
+                            {
+                                reader.Read();
+                                place = reader.Value;
+                            }
+                            else if (reader.Name.Equals("observation_time"))
+                            {
+                                reader.Read();
+                                observation_time = reader.Value;
+                            }
+                            else if (reader.Name.Equals("weather"))
+                            {
+                                reader.Read();
+                                weather1 = reader.Value;
+                            }
+                            else if (reader.Name.Equals("temperature_string"))
+                            {
+                                reader.Read();
+                                temperature_string = reader.Value;
+                            }
+                            else if (reader.Name.Equals("relative_humidity"))
+                            {
+                                reader.Read();
+                                relative_humidity = reader.Value;
+                            }
+                            else if (reader.Name.Equals("wind_string"))
+                            {
+                                reader.Read();
+                                wind_string = reader.Value;
+                            }
+                            else if (reader.Name.Equals("pressure_mb"))
+                            {
+                                reader.Read();
+                                pressure_mb = reader.Value;
+                            }
+                            else if (reader.Name.Equals("dewpoint_string"))
+                            {
+                                reader.Read();
+                                dewpoint_string = reader.Value;
+                            }
+                            else if (reader.Name.Equals("visibility_km"))
+                            {
+                                reader.Read();
+                                visibility_km = reader.Value;
+                            }
+                            else if (reader.Name.Equals("latitude"))
+                            {
+                                reader.Read();
+                                latitude = reader.Value;
+                            }
+                            else if (reader.Name.Equals("longitude"))
+                            {
+                                reader.Read();
+                                longitude = reader.Value;
+                            }
+
+                            break;
+                    }
+                }
+            }
+
+            //Display each of the variables that were obtained through the parsing
+            ResultsLbl1.Text = place;
+            ResultsLbl2.Text = observation_time;
+            ResultsLbl3.Text = weather1;
+            ResultsLbl4.Text = temperature_string;
+            ResultsLbl5.Text = relative_humidity;
+            ResultsLbl6.Text = wind_string;
+            ResultsLbl7.Text = pressure_mb;
+            ResultsLbl8.Text = dewpoint_string;
+            ResultsLbl9.Text = visibility_km;
+            ResultsLbl10.Text = longitude + ", " + latitude;
+        }
+    }
+}
